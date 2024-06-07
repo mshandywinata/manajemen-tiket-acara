5373515ea0df03d81a71f23a0e270d62d6315826
Fauziahz <fauziahzahraap.31@upi.edu>
2024-06-07 23:22:42 +0700

    add fiturtambahhapuslagu

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <sstream>
 #include <conio.h>
+#include <vector>
+#include <algorithm>
 #include <iomanip> // for std::setw
 
 using namespace std;
@@ -12,6 +14,15 @@ struct dataPengguna
     string username;
     string password;
 };
+
+class User
+{
+public:
+    string username;
+    string password;
+};
+
+// FITUR REGIS LOGIN ===========================================================
 
 void displayInput(const string &inputan, string &inputValue)
 {
@@ -26,7 +37,8 @@ void displayInput(const string &inputan, string &inputValue)
 void displayPasswordPrompt(string &password)
 {
     cout << "+-----------------------------+\n";
-    cout << "| " << left << setw(27) << "Masukkan password" << " |\n";
+    cout << "| " << left << setw(27) << "Masukkan password"
+         << " |\n";
     cout << "+-----------------------------+\n";
     cout << "| ";
 
@@ -50,17 +62,15 @@ void displayPasswordPrompt(string &password)
     cout << "\n+-----------------------------+\n";
 }
 
-void registerUser()
+void registerUser(User &user)
 {
-    string username, password;
+    displayInput("username", user.username);
+    displayPasswordPrompt(user.password);
 
-    displayInput("username", username);
-    displayPasswordPrompt(password);
-
-    ofstream file("users.csv", ios::app);
+    ofstream file("output/users.csv", ios::app);
     if (file.is_open())
     {
-        file << username << "," << password << endl;
+        file << user.username << "," << user.password << endl;
         file.close();
         cout << "\nRegistrasi berhasil" << endl;
     }
@@ -70,14 +80,12 @@ void registerUser()
     }
 }
 
-bool loginUser()
+bool loginUser(User &user)
 {
-    string username, password;
+    displayInput("username", user.username);
+    displayPasswordPrompt(user.password);
 
-    displayInput("username", username);
-    displayPasswordPrompt(password);
-
-    ifstream file("users.csv");
+    ifstream file("output/users.csv");
     if (file.is_open())
     {
         string line, dataUsername, dataPassword;
@@ -88,10 +96,10 @@ bool loginUser()
             getline(ss, dataUsername, ',');
             getline(ss, dataPassword, ',');
 
-            if (dataUsername == username)
+            if (dataUsername == user.username)
             {
                 dataUserDitemukan = true;
-                if (dataPassword == password)
+                if (dataPassword == user.password)
                 {
                     file.close();
                     cout << "\nLogin berhasil" << endl;
@@ -119,8 +127,248 @@ bool loginUser()
     return false;
 }
 
+// FITUR TAMBAH HAPUS LAGU ===========================================================
+
+const int MAKS_QUEUE = 100;
+
+struct Lagu
+{
+    string judul;
+    string artis;
+    string genre;
+};
+
+struct Queue
+{
+    int front, rear;
+    Lagu isi[MAKS_QUEUE];
+} queue;
+
+void createQueue()
+{
+    queue.front = queue.rear = -1;
+}
+
+bool isEmpty()
+{
+    return queue.front == -1;
+}
+
+bool isFull()
+{
+    return (queue.rear + 1) % MAKS_QUEUE == queue.front;
+}
+
+void enqueue(const Lagu &lagu)
+{
+    if (isFull())
+    {
+        cout << "Antrian penuh, lagu tidak dapat ditambahkan." << endl;
+    }
+    else
+    {
+        if (isEmpty())
+        {
+            queue.front = queue.rear = 0;
+        }
+        else
+        {
+            queue.rear = (queue.rear + 1) % MAKS_QUEUE;
+        }
+        queue.isi[queue.rear] = lagu;
+    }
+}
+
+void dequeue()
+{
+    if (isEmpty())
+    {
+        cout << "Antrian kosong, tidak ada lagu yang dihapus." << endl;
+    }
+    else
+    {
+        cout << "Menambah lagu: " << queue.isi[queue.front].judul << ", "
+             << queue.isi[queue.front].artis << ", "
+             << queue.isi[queue.front].genre << endl;
+        if (queue.front == queue.rear)
+        {
+            queue.front = queue.rear = -1;
+        }
+        else
+        {
+            queue.front = (queue.front + 1) % MAKS_QUEUE;
+        }
+    }
+}
+
+void tambahLaguKeCsv(const string &username)
+{
+    string filename = "dataLaguUser/" + username + ".csv";
+    ofstream file(filename, ios::app);
+
+    if (!file.is_open())
+    {
+        cout << "Tidak dapat membuka file untuk menambahkan data." << endl;
+        return;
+    }
+
+    while (!isEmpty())
+    {
+        file << queue.isi[queue.front].judul << ","
+             << queue.isi[queue.front].artis << ","
+             << queue.isi[queue.front].genre << "\n";
+        dequeue();
+    }
+
+    file.close();
+    cout << "Lagu-lagu berhasil ditambahkan ke dalam file CSV untuk pengguna " << username << "\n"
+         << endl;
+}
+
+bool cekJudul(const string &judul, const string &filename)
+{
+    ifstream file(filename);
+    if (!file.is_open())
+    {
+        cout << "Tidak dapat membuka file " << filename << endl;
+        return false;
+    }
+
+    string line;
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        string judulLagu;
+
+        getline(ss, judulLagu, ',');
+        if (judulLagu == judul)
+        {
+            file.close();
+            return true;
+        }
+    }
+
+    file.close();
+    return false;
+}
+
+void displayQueue(const string &filename)
+{
+    ifstream file(filename);
+
+    if (!file.is_open())
+    {
+        cout << "Tidak dapat membuka file " << filename << endl;
+        return;
+    }
+
+    string line;
+    int index = 1;
+    bool isEmpty = true;
+    cout << "Isi Queue:" << endl;
+    while (getline(file, line))
+    {
+        isEmpty = false;
+        stringstream ss(line);
+        string judul, artis, genre;
+        getline(ss, judul, ',');
+        getline(ss, artis, ',');
+        getline(ss, genre, ',');
+        cout << "[" << index++ << "] " << judul << " - " << artis << " - " << genre << endl;
+    }
+
+    if (isEmpty)
+    {
+        cout << "File kosong, tekan enter untuk kembali" << endl;
+    }
+
+    file.close();
+}
+
+void deleteQueue(const string &judul, const string &filename)
+{
+    ifstream file(filename);
+    if (!file.is_open())
+    {
+        cout << "Tidak dapat membuka file " << filename << "\n"
+             << endl;
+        return;
+    }
+
+    vector<Lagu> laguList;
+    string line;
+    bool found = false;
+
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        string judulLagu, artis, genre;
+        getline(ss, judulLagu, ',');
+        getline(ss, artis, ',');
+        getline(ss, genre, ',');
+
+        if (judulLagu == judul)
+        {
+            found = true;
+            cout << "Lagu " << judul << " sudah keluar dari queue.\n"
+                 << endl;
+        }
+        else
+        {
+            laguList.push_back({judulLagu, artis, genre});
+        }
+    }
+
+    file.close();
+
+    if (!found)
+    {
+        cout << "Data tidak ditemukan\n"
+             << endl;
+        return;
+    }
+
+    ofstream outFile(filename);
+
+    for (const auto &lagu : laguList)
+    {
+        outFile << lagu.judul << "," << lagu.artis << "," << lagu.genre << "\n";
+    }
+
+    outFile.close();
+}
+
+Lagu getLaguInfo(const string &judul, const string &filename)
+{
+    ifstream file(filename);
+    if (!file.is_open())
+    {
+        cout << "Tidak dapat membuka file " << filename << endl;
+        return {"", "", ""};
+    }
+
+    string line;
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        string judulLagu, artis, genre;
+
+        getline(ss, judulLagu, ',');
+        if (judulLagu == judul)
+        {
+            getline(ss, artis, ',');
+            getline(ss, genre, ',');
+            return {judulLagu, artis, genre};
+        }
+    }
+
+    file.close();
+    return {"", "", ""};
+}
+
 void displayMenu()
 {
+    system("cls");
     cout << "\n=== Selamat Datang di Aplikasi ===\n";
     cout << "+----+------------------------+\n";
     cout << "| No | Pilihan menu kami      |\n";
@@ -138,6 +386,7 @@ int main()
 {
     int pilihan;
     bool sedangLogin = false;
+    User user;
 
     do
     {
@@ -146,17 +395,129 @@ int main()
 
         if (pilihan == 1)
         {
-            registerUser();
+            registerUser(user);
         }
         else if (pilihan == 2)
         {
-            if (loginUser())
+            if (loginUser(user))
             {
                 sedangLogin = true;
+
+                system("cls");
+                createQueue();
+
+                string filename = "dataLaguUser/" + user.username + ".csv"; // Nama file CSV sesuai username
+
+                while (true)
+                {
+                    system("cls");
+                    cout << endl;
+                    cout << "        === MUSIKKU ===       " << endl;
+                    cout << "+----+------------------------+\n";
+                    cout << "| No | Pilihan menu kami      |\n";
+                    cout << "+----+------------------------+\n";
+                    cout << "| 1  | Tambahkan Lagu         |\n";
+                    cout << "+----+------------------------+\n";
+                    cout << "| 2  | Hapus Lagu             |\n";
+                    cout << "+----+------------------------+\n";
+                    cout << "| 3  | Keluar                 |\n";
+                    cout << "+----+------------------------+\n";
+                    cout << "Pilih opsi: ";
+
+                    string opsiMenu;
+                    getline(cin, opsiMenu);
+
+                    if (opsiMenu == "1")
+                    {
+                        while (true)
+                        {
+                            system("cls");
+                            cout << "     === TAMBAHKAN LAGU ===     " << endl;
+
+                            string judul;
+                            cout << "+-----------------------------+\n";
+                            cout << "| " << left << setw(27) << ("Masukkan Judul Lagu") << " |\n";
+                            cout << "+-----------------------------+\n";
+                            cout << "| ";
+                            getline(cin, judul);
+                            cout << "+-----------------------------+\n";
+
+                            if (judul.empty())
+                            {
+                                cout << "Input tidak boleh kosong" << endl;
+                                system("pause");
+                            }
+                            else
+                            {
+                                if (cekJudul(judul, "dataLagu.csv")) // Ganti dengan nama file yang berisi data lagu
+                                {
+                                    Lagu lagu = getLaguInfo(judul, "dataLagu.csv");
+                                    enqueue(lagu);
+                                    tambahLaguKeCsv(user.username);
+                                }
+                                else
+                                {
+                                    cout << "Judul lagu tidak ditemukan dalam database." << endl;
+                                }
+                                system("pause");
+                                break;
+                            }
+                        }
+                    }
+                    else if (opsiMenu == "2")
+                    {
+                        system("cls");
+                        while (true)
+                        {
+                            ifstream file(filename);
+
+                            cout << "       === HAPUS LAGU ===     \n"
+                                 << endl;
+
+                            displayQueue(filename);
+                            cout << endl;
+
+                            if (!file.is_open())
+                            {
+                                system("pause");
+                                break;
+                            }
+
+                            string judul;
+                            cout << "+-----------------------------+\n";
+                            cout << "| " << left << setw(27) << ("Masukkan Judul Lagu") << " |\n";
+                            cout << "+-----------------------------+\n";
+                            cout << "| ";
+                            getline(cin, judul);
+                            cout << "+-----------------------------+\n";
+
+                            if (judul.empty())
+                            {
+                                system("pause");
+                                break;
+                            }
+                            else
+                            {
+                                deleteQueue(judul, filename);
+                                system("pause");
+                                break;
+                            }
+                        }
+                    }
+                    else if (opsiMenu == "3")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        cout << "Input tidak valid!" << endl;
+                    }
+                }
             }
         }
         else if (pilihan == 3)
         {
+            system("cls");
             cout << "Keluar dari program.\n";
         }
         else
